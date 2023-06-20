e937a8805fade5fdf59497e6bd1ef74fad2cc809
sundar chen <sundarchen@126.com>
2023-06-20 16:46:34 +0800

    协仪整理；

--- lib/common/nets/cmds.dart
+++ lib/common/nets/cmds.dart
@@ -27,25 +27,25 @@ class CMD {
   // =========房间相关===========
   static const S_LiveBroadcast = 2200; // 房间开关播广播
 
-  static const S_JoinBroadcast = 2200; // 加入房间广播 【对应旧mq类型:1】
-  static const S_LeaveBroadcast = 2201; // 退出房间广播 【对应旧mq类型:2】
-  static const S_InviteMikeBroadcast = 2202; // 邀请上麦广播 【对应旧mq类型：3】
-  static const S_UpMikeBroadcast = 2203; // 上麦广播 【对应旧mq类型：4】
-  static const S_DownMikeBroadcast = 2204; // 下麦麦广播 【对应旧mq类型：5】
+  static const S_JoinBroadcast = 2210; // 加入房间广播 【对应旧mq类型:1】
+  static const S_LeaveBroadcast = 2211; // 退出房间广播 【对应旧mq类型:2】
+  static const S_InviteMikeBroadcast = 2222; // 邀请上麦广播 【对应旧mq类型：3】
+  static const S_UpMikeBroadcast = 2220; // 上麦广播 【对应旧mq类型：4】
+  static const S_DownMikeBroadcast = 2221; // 下麦麦广播 【对应旧mq类型：5】
   static const S_NoticeBroadcast = 2240; // 房间公告广播 【对应旧mq类型：6】
 
   static const S_GiftPlay = 3104; // 礼物播放广播(对应mq的8)
-  static const S_AdministratorBroadcast = 2208; // 房间设置管理员广播 【对应旧mq类型：9】
+  static const S_AdministratorBroadcast = 2280; // 房间设置管理员广播 【对应旧mq类型：9】
 
   static const S_BlackBroadcast = 2250; // 房间黑名单广播【对应旧mq类型：11】
-  static const S_CloseMikeBroadcast = 22010; // 禁麦广播 【对应旧mq类型：12】
-  static const S_OpenBroadcast = 22011; // 开麦广播 【对应旧mq类型：13】
-  static const S_ApplyUpMikeBroadcast = 22012; // 申请上麦 【对应旧mq类型：14】
+  static const S_CloseMikeBroadcast = 2223; // 禁麦广播 【对应旧mq类型：12】
+  static const S_OpenBroadcast = 2224; // 开麦广播 【对应旧mq类型：13】
+  static const S_ApplyUpMikeBroadcast = 2225; // 申请上麦 【对应旧mq类型：14】
 
   static const S_UpdateLevel = 3102; // 等级提升广播（对应mq的16）
-  static const S_LiveStopBroadcast = 22015; // 房间关播广播【对应旧mq类型：17】
+  static const S_LiveStopBroadcast = 2201; // 房间关播广播【对应旧mq类型：17】
   static const S_AccMikeBroadcast = 2230; // 房间计数器广播, 18
-  static const S_UpdateCharmLevel = 3103; // 魅力等级提升广播（对应mq的19）
+  static const S_UpdateCharmLevel = 2270; // 魅力等级提升广播（对应mq的19）
   static const S_ChatMessageBroadcast = 2270; // 房间聊天信息广播
 
 
