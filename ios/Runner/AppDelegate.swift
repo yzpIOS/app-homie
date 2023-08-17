af648208b8601fe5e6f639cfd5c45d777da9a303
sundar chen <sundarchen@126.com>
2023-08-17 16:49:56 +0800

    openinstaall对接；

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -24,14 +24,14 @@ import openinstall_flutter_plugin
     override func applicationDidBecomeActive(_ application: UIApplication) {
         UnityFramework.getInstance().pause(false);
     }
-    
+
     //2）首次集成插件的用户，如果拉起无法获取到参数，是因为方法被其它插件覆盖导致（openinstall插件不会覆盖其它插件），可以有两种方法解决：
     override func application(_ application: UIApplication, willContinueUserActivityWithType userActivityType: String) -> Bool {
         //处理通过openinstall一键唤起App时传递的数据
         OpeninstallFlutterPlugin.continue(userActivity)
         return true;
     }
-    
+
     //第二种方法，将所有插件的拉起代理方法的逻辑，统一在AppDelegate文件中来处理，代码如下：
     override func application(_ application: UIApplication, handleOpen url: URL) -> Bool {
         OpeninstallFlutterPlugin.handLinkURL(url)
