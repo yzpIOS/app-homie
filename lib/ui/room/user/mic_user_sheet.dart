acbdc8407f714686e9ec278e4134929c6023ded1
sundar chen <sundarchen@126.com>
2023-09-14 15:35:01 +0800

    1、bug处理；

--- lib/ui/room/user/mic_user_sheet.dart
+++ lib/ui/room/user/mic_user_sheet.dart
@@ -12,92 +12,45 @@ import 'package:fixnum/fixnum.dart';
 
 abstract class MicUserSheet {
   static void show(String no, {MicInfo? info}) {
-    // final roomCtrl = Get.find<RoomManagerCtrl>().sceneCtrl;
-    //
-    // final isRoom = roomCtrl is RoomCtrl;
-    //
-    // final roomId = roomCtrl.roomId;
-    // final myUid = OAuthCtrl.uid;
-    // final posUid = info?.uid;
-    // final posnUid = info?.nUid;
-    // final nUid = info?.nUid;
-    //
-    // final isAdmin = isRoom && roomCtrl.isAdmin(myUid);
-    // final isOwner = isRoom && roomCtrl.isOwner(myUid);
-    // final canManage = isOwner || isAdmin;
-    //
-    // final notUser = posUid == null;
-    // final hasUser = !notUser;
-    //
-    // final isSelf = hasUser && posUid == myUid;
-    // final notSelf = !isSelf;
-    //
-    // final posAdmin = isRoom && hasUser && roomCtrl.isAdmin(posUid);
-    // final posOwner = isRoom && hasUser && roomCtrl.isOwner(posUid);
-    // final posManage = posAdmin || posOwner;
-    //
-    // final actions = <String, FutureOr Function()>{
-    //   if (hasUser && notSelf) //
-    //     '查看资料': () {
-    //       if(posnUid == null) {
-    //         return;
-    //       }
-    //       RoomUserInfoDialog.show(uid: posUid);
-    //     },
-    //   if (hasUser && canManage && notSelf) //
-    //     '抱Ta下麦': () {
-    //       sceneMicCtrl<RoomMicCtrl>().micDow(alert: '确定抱Ta下麦', no: no);
-    //     },
-    //   if (notUser && canManage) //
-    //     '抱Ta上麦': () {
-    //       Get.to(() => SelectUserPage(roomId: roomId, title: '抱Ta上麦')) //
-    //           ?.onType<Map>((val) {
-    //         sceneMicCtrl<RoomMicCtrl>().inviteMicUp(no: no, uid: Int64(val['role_id']));
-    //       });
-    //     },
-    //   if (hasUser && canManage) //
-    //     '清空魅力值': () {
-    //       simpleSub(Api.Room.resetHotCount(roomId: roomId, micId: info!.micId));
-    //     },
-    //   if (hasUser && canManage && notSelf && !posManage) //
-    //     '加入黑名单': () {
-    //       if(posnUid == null) {
-    //         return;
-    //       }
-    //       roomCtrl.setBlock(uid: posUid, isAdd: true);
-    //     },
-    //   if (hasUser && isOwner && notSelf && !posManage) //
-    //     '添加管理员': () {
-    //       roomCtrl.setManager(uid: posUid, isAdd: true);
-    //     },
-    //   if (hasUser && posAdmin && isOwner && notSelf) //
-    //     '移除管理员': () {
-    //       roomCtrl.setManager(uid: posUid, isAdd: false);
-    //     },
-    //   if (notUser) //
-    //     '移到此座位': () {
-    //       sceneMicCtrl<RoomMicCtrl>().micUp(no: no);
-    //     },
-    //   if (isSelf) //
-    //     '下麦旁听': () {
-    //       // sceneMicCtrl<RoomMicCtrl>().micDow(alert: '确定下麦', no: no);
-    //       RoomUserInfoDialog.show(uid: posUid);
-    //     },
-    // };
-    //
-    // switch (actions.length) {
-    //   case 0:
-    //     break;
-    //   case 1:
-    //     simpleTry(actions.values.single);
-    //     break;
-    //   default:
-    //     Get.showSheet(actions.entries, toTitle: (it) => Tuple2(it.key, null)) //
-    //         .onNotNull((val) => simpleTry(val.value));
-    // }
     if(info == null) {
-      // 用户上麦
-      sceneMicCtrl<RoomMicCtrl>().micUp(no: no);
+      final roomCtrl = Get.find<RoomManagerCtrl>().sceneCtrl;
+      // 是否在房
+      final isRoom = roomCtrl is RoomCtrl;
+      // 房间信息
+      final roomId = roomCtrl.roomId;
+      final myUid = OAuthCtrl.uid;
+      // 是否是管理员
+      final isAdmin = isRoom && roomCtrl.isAdmin(myUid);
+      // 是否是管理者
+      final isOwner = isRoom && roomCtrl.isOwner(myUid);
+      final canManage = isOwner || isAdmin;
+      if(canManage) {
+        // 管理员操作
+        final actions = <String, FutureOr Function()>{
+          '抱Ta上麦': () {
+            Get.to(() => SelectUserPage(roomId: roomId, title: '抱Ta上麦')) //
+                ?.onType<Map>((val) {
+              sceneMicCtrl<RoomMicCtrl>().inviteMicUp(no: no, uid: Int64(val['role_id']));
+            });
+          },
+          '移到此座位': () {
+            sceneMicCtrl<RoomMicCtrl>().micUp(no: no);
+          },
+        };
+        switch (actions.length) {
+          case 0:
+            break;
+          case 1:
+            simpleTry(actions.values.single);
+            break;
+          default:
+            Get.showSheet(actions.entries, toTitle: (it) => Tuple2(it.key, null)) //
+                .onNotNull((val) => simpleTry(val.value));
+        }
+      } else {
+        // 普通用户的操作
+        sceneMicCtrl<RoomMicCtrl>().micUp(no: no);
+      }
     } else {
       // 显示用户信息
       RoomUserInfoDialog.show(uid: info.uid, nuid: info.nUid, micNo: no);
