86cda7cf65785ff0a555b42f409113c29188b80c
sundar chen <sundarchen@126.com>
2023-09-27 10:44:38 +0800

    1、添加测方式服务器；

--- apk_dev.sh
+++ apk_dev.sh
@@ -1,3 +1,3 @@
 #flutter clean && flutter pub get
-#flutter build apk --split-per-abi --target-platform android-arm64 --bundle-sksl-path ./gen/flutter_01.sksl.json --dart-define-from-file=./env/dev.json --dart-define=use_proxy=true
+#flutter build apk --split-per-abi --target-platform android-arm64 --bundle-sksl-path ./gen/flutter_01.sksl.json --dart-define-from-file=./env/test.json --dart-define=use_proxy=true
 flutter build apk --split-per-abi --target-platform android-arm64 --dart-define-from-file=./env/dev.json --dart-define=use_proxy=true
