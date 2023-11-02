d7b104abc6250dc43551c14413d4420a0465b772
Wangyaxiong <tinyxion@qq.com>
2023-11-02 20:01:54 +0800

    Update my_user_init_perfect_info_page.dart

--- lib/ui/login/init/my_user_init_perfect_info_page.dart
+++ lib/ui/login/init/my_user_init_perfect_info_page.dart
@@ -42,7 +42,7 @@ class _MyUserInitPerfectInfoPageState extends State<MyUserInitPerfectInfoPage> {
     return WillPopScope(
       onWillPop: () => Future.value(true),
       child: Scaffold(
-        appBar: xAppBar(bgColor: AppPalette.transparent),
+        appBar: AppBar(backgroundColor: AppPalette.transparent),
         extendBodyBehindAppBar: true,
         resizeToAvoidBottomInset: false,//是否调整大小以避免底部嵌入(防止下一步按钮上移)
         body: Stack(
