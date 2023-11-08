caf4b55b1b7cd39f54b11bab39be01da4b72f6c4
sundar chen <sundarchen@126.com>
2023-11-08 10:50:22 +0800

    1、删除不必要代码；

--- lib/ui/moment/index/moment_view.dart
+++ lib/ui/moment/index/moment_view.dart
@@ -2,7 +2,6 @@ import 'package:app/event/event.dart';
 import 'package:app/model/api/moment_dto.dart';
 import 'package:app/store/moment/moment_ctrl.dart';
 import 'package:app/tools.dart';
-import 'package:app/ui/upgrade/upgrade_page.dart';
 import 'package:app/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -99,10 +98,6 @@ class _ItemView extends ViewAdapter<MomentDtoAdapter>
     child = DecorButton(
       child: child,
       onTap: () {
-        if(Env.isDebug) {
-          UpGradePage.showDialog();
-          return;
-        }
         callBack.call(vm);
       },
     );
