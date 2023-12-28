3e4ad4cefddb8169f2d634cbd3790ff367f9cbf5
sundar chen <sundarchen@126.com>
2023-12-29 04:22:56 +0800

    1

--- lib/ui/room/persion/person_room_mic_ctrl.dart
+++ lib/ui/room/persion/person_room_mic_ctrl.dart
@@ -73,23 +73,24 @@ class PersonRoomMicCtrl extends RoomMicCtrl  {
       MicInfo? micInfo = micUserList.firstWhereOrNull((element) => element.uid == event.uid);
       // 是否禁麦中
       micInfo?.isMute = event.data?.status == 1;
-      if(event.data?.status == 1) {
-        if(Rtc.micRx.isTrue && event.uid == OAuthCtrl.uid) {
-          // 禁麦中
-          Rtc.micSwitch();
-          Rtc.status.value = PersonMicStatus.disable.val;
-          Rtc.micRx.value = false;
-        }
-      } else {
-        if(micInfo != null) {
-          // 当前用户在麦上，改成开麦
-          if(Rtc.micRx.isFalse && event.uid == OAuthCtrl.uid) {
-            Rtc.status.value = PersonMicStatus.open.val;
+
+      if(event.uid == OAuthCtrl.uid) {
+        if(event.data?.status == 1) {
+          if(Rtc.micRx.isTrue) {
             Rtc.micSwitch();
-            Rtc.micRx.value = true;
           }
+          // 禁麦中
+          Rtc.status.value = PersonMicStatus.disable.val;
         } else {
-          Rtc.status.value = PersonMicStatus.none.val;
+          if(micInfo != null) {
+            // 当前用户在麦上，改成开麦
+            if(Rtc.micRx.isFalse) {
+              Rtc.micSwitch();
+            }
+            Rtc.status.value = PersonMicStatus.open.val;
+          } else {
+            Rtc.status.value = PersonMicStatus.none.val;
+          }
         }
       }
       if(micInfo != null) {
