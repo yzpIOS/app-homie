98ea03affea28a66ec2263746cb80c9b0f74ccc5
sundar chen <sundarchen@126.com>
2024-01-18 17:21:43 +0800

    1

--- lib/ui/my/invite_new_members/invite_new_members_myrewardlist_sheet.dart
+++ lib/ui/my/invite_new_members/invite_new_members_myrewardlist_sheet.dart
@@ -83,7 +83,7 @@ class _InviteNewMembersMyRewardListSheetState extends State<InviteNewMembersMyRe
             child: child
         ),
         Positioned(
-          top: 10,
+          top: 12,
           child: Image.asset(IMG.format('my/invite_new_members_sheet_header'), scale: 3),
         ),
         Positioned(
