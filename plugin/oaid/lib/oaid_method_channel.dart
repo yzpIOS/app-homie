9ddd4e878e25cf2a41a03ca465088d41c460352b
sundar chen <sundarchen@126.com>
2024-03-14 13:58:15 +0800

    1、解决报错；

--- plugin/oaid/lib/oaid_method_channel.dart
+++ plugin/oaid/lib/oaid_method_channel.dart
@@ -8,7 +8,7 @@ import 'oaid_platform_interface.dart';
 /// An implementation of [OaidPlatform] that uses method channels.
 class MethodChannelOaid extends OaidPlatform {
   /// The method channel used to interact with the native platform.
-  @visibleForTesting
+
   final methodChannel = const MethodChannel('oaid');
 
   @override
