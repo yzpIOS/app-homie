094a5aaf8d17e0c98779e2bb682c72cd1cea0f09
sundar chen <sundarchen@126.com>
2024-03-18 16:28:45 +0800

    1、我的装扮衣柜数据bug处理;

--- lib/net/api/api_dressup.dart
+++ lib/net/api/api_dressup.dart
@@ -131,7 +131,7 @@ class ApiDressUp extends ApiBase {
   Future backpackList2({List? categories = null}) {
     return _doPost('backpack/query', data: {
       if(categories != null)
-        "category_id_list": categories,
+        "group_id_list": categories,
     }).then((val) => val['items']);
   }
 
