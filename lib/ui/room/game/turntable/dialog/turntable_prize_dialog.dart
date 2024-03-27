51b9f99296721a50957da482ecdd3127013457a4
sundar chen <sundarchen@126.com>
2024-03-27 18:51:08 +0800

    1、细节优化;

--- lib/ui/room/game/turntable/dialog/turntable_prize_dialog.dart
+++ lib/ui/room/game/turntable/dialog/turntable_prize_dialog.dart
@@ -4,12 +4,15 @@ import 'package:app/tools.dart';
 import 'package:app/tools/view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:svgaplayer_flutter/svgaplayer_flutter.dart';
 
 class TurntablePrizeDialog extends StatefulWidget {
 
   List items;
 
-  TurntablePrizeDialog(this.items, {super.key});
+  MovieEntity movieEntity;
+
+  TurntablePrizeDialog(this.items, this.movieEntity, {super.key});
 
   static Future<void> showDialog(List items) async {
     items.sort((a, b) {
@@ -17,7 +20,9 @@ class TurntablePrizeDialog extends StatefulWidget {
       int value2 = (b["price"] ?? 0);
       return value2 - value1;
     });
-    var dialog = TurntablePrizeDialog(items);
+
+    MovieEntity movieEntity = await SVGAParser.shared.decodeFromAssets('assets/烟花.svga');
+    var dialog = TurntablePrizeDialog(items, movieEntity);
     await Get.dialog(
       dialog,
       useSafeArea: false,
@@ -29,9 +34,27 @@ class TurntablePrizeDialog extends StatefulWidget {
   State<StatefulWidget> createState() => _TurntablePrizeDialogState();
 }
 
-class _TurntablePrizeDialogState extends State<TurntablePrizeDialog> {
+class _TurntablePrizeDialogState extends State<TurntablePrizeDialog> with TickerProviderStateMixin {
 
   int index = 0;
+
+  _AnimeHelp? anime = null;
+
+  @override
+  void initState() {
+    super.initState();
+    anime = _AnimeHelp(this, widget.movieEntity);
+
+    delay(300, () async {
+      await anime?.doStart(
+        callback: () async {
+          anime?.dispose();
+          anime = null;
+          setState(() { });
+        },
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,6 +73,9 @@ class _TurntablePrizeDialogState extends State<TurntablePrizeDialog> {
             createCongratulations(),
 
             createBottomButton(),
+
+            // 烟花
+            createFireWorks(),
           ],
         ),
       ),
@@ -186,5 +212,82 @@ class _TurntablePrizeDialogState extends State<TurntablePrizeDialog> {
         ),
       ),
     );
+  }
+
+  Widget createFireWorks() {
+    if(anime?.ctrl == null) {
+      return SizedBox();
+    }
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      top: 0,
+      child: RepaintBoundary(
+        child: SVGAImage(
+          anime!.ctrl,
+          fit: BoxFit.cover,
+          clearsAfterStop: false,
+          allowDrawingOverflow: true,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    anime?.dispose();
+    anime = null;
+  }
+}
+
+
+
+class _AnimeHelp {
+  final SVGAAnimationController ctrl;
+
+  _AnimeHelp(TickerProvider vsync, MovieEntity entity)
+      : ctrl = SVGAAnimationController(vsync: vsync)..videoItem = entity;
+
+  late final frames = ctrl.frames;
+  late final dur = ctrl.duration ?? const Duration(seconds: 1);
+  late final step = (frames - 20) / frames;
+
+  bool _isDispose = false;
+
+  Future<void> doStart({required Future Function() callback}) async {
+    bool running = true;
+
+    final anim = Future.doWhile(
+          () async {
+        await ctrl.animateTo(step, duration: dur * step).orCancel;
+        return running;
+      },
+    );
+
+    try {
+      if (_isDispose) return;
+
+      running = false;
+
+      await anim;
+      if (_isDispose) return;
+
+      await ctrl.fling().orCancel.catchError((_) {});
+      if (_isDispose) return;
+
+      await callback();
+    } finally {
+      if (!_isDispose) ctrl.reset();
+    }
+  }
+
+  void dispose() {
+    _isDispose = true;
+
+    ctrl
+      ..stop()
+      ..dispose();
   }
 }
