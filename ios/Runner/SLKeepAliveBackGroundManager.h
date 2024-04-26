9506975e3cf3de46968fb6d8c3bd2fefe9ed3be4
sundar chen <sundarchen@126.com>
2024-04-26 14:54:16 +0800

    1、删除无用注释；

--- ios/Runner/SLKeepAliveBackGroundManager.h
+++ ios/Runner/SLKeepAliveBackGroundManager.h
@@ -2,7 +2,7 @@
 ////  SLKeepAliveBackGroundManager.h
 ////  SangoLive
 ////
-////  Created by 杨瑞琪 on 2023/7/10.
+////  Created by  on 2023/7/10.
 ////  Copyright © 2023 Sango. All rights reserved.
 ////
 //
